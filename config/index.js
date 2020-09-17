ad4bcc6c0a8332a43aa54e00bcab682dc90a6b29
Will <will.hanlen@gmail.com>
2020-09-17 12:22:18 -0500

    update the environment variable being read to decide if the app is on localhost or not
    on vercel, the app is currently not deployed on a production branch so there is no NODE_ENV production variable set. This breaks the build. Fixed by changing the environment variable that is being read

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
-const dev = process.env.NODE_ENV !== 'production';
+const dev = (process.env.VER_ENV !== 'prod' || process.env.NODE_ENV !== 'production');
 
 export const server = dev ? 'http://localhost:3000' : 'https://capsulate.vercel.app';
