f99c910c2914ea969ec101c1f6cfc603cdadd8ec
Will <will.hanlen@gmail.com>
2020-09-17 17:32:43 -0500

    change 'database' entries to be about my own life
    This makes it look more like a real app and also gives some insight into me as a person.

--- pages/api/journal.js
+++ pages/api/journal.js
@@ -3,32 +3,50 @@ export default (req, res) => {
     {
       id: 1,
       date: "2019-01-03",
-      text: "My first upload",
+      text: "My first upload. This post does not have a photo.",
       photo: ""
     },
     {
       id: 2,
-      date: "2019-01-05",
-      text: "Today was a fun day. I went to the beach.",
-      photo: "https://i.ytimg.com/vi/rVSDFk3vY5c/maxresdefault.jpg"
+      date: "2018-09-05",
+      text: "Did a triathalon in San Diego California.",
+      photo: "https://drive.google.com/uc?export=download&id=0ByJhUz9RiNenT05sdUVaTGZkTDQ"
     },
     {
       id: 3,
-      date: "2019-01-09",
-      text: "Today was rainy.",
-      photo: "https://cdna.artstation.com/p/assets/covers/images/020/320/442/large/yevi-art-regenraupe.jpg?1567330786"
+      date: "2018-11-25",
+      text: "Christmas back home in Kansas. It's good to have some time off.",
+      photo: "https://drive.google.com/uc?export=download&id=1QfuZMhMlgcKgoKbJu7gLKigwBqnfhrRY"
     },
     {
-      id: 4,
-      date: "2019-01-15",
-       text: "I got a new puppy today.",
-      photo: ""
+      id: 7,
+      date: "2019-03-15",
+      text: "Spring break in Cancun. My $3 sunglasses are looking very cool.",
+      photo: "https://drive.google.com/uc?export=download&id=1Mkd8tPXCmviqEB3MnHKNgu1_209nZR3E"
     },
     {
       id: 5,
-      date: "2020-01-15",
-      text: "My puppy is a year old today.",
-      photo: "https://i.pinimg.com/originals/70/98/9e/70989ecb0258f566b9af13e876cf7dce.gif"
+      date: "2019-03-12",
+      text: "Combat dining in with Nick.",
+      photo: "https://drive.google.com/uc?export=download&id=1hKs695bKTj2MUFrzjbxKCO7uFffan8yH"
+    },
+    {
+      id: 4,
+      date: "2019-07-15",
+       text: "Commitment dinner at USAFA.",
+      photo: "https://drive.google.com/uc?export=download&id=1q1_1hZfEJH9UX5F7jRhmoGEULprMB-45"
+    },
+    {
+      id: 6,
+      date: "2020-00-01",
+      text: "Scuba diving in Thialand. Saw a whale shark!",
+      photo: "https://drive.google.com/uc?export=download&id=1P3WjJFHPWkq-tTe38pdGBse1RqhWAAzs"
+    },
+    {
+      id: 8,
+      date: "2020-07-22",
+      text: "Cheesing big.",
+      photo: "https://drive.google.com/uc?export=download&id=1DGoURHjY_71WvoPJlnKKTsoOVtPUpF5i"
     },
   ]
 
